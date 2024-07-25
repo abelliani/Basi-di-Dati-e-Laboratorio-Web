a85785984aea0808b70cbf2c5d2c8301444c0e29
abelliani <125980058+abelliani@users.noreply.github.com>
2024-07-25 18:30:01 +0200

    Delete Progetto directory

--- Progetto/596864/connessione.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $mysqli = new mysqli('localhost', 'root', '', 'PROGETTO596864');
-    if ($mysqli->connect_error){
-        die ('Connessione fallita:' . $mysqli->connect_errno . ' - ' . $mysqli->connect_error);
-    }
-?>
